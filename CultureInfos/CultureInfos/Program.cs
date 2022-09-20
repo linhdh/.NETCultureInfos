1e65a0c797b42ee8238a81ccecf16b085aa2a8bd
Dao Hai Linh <linhdh@live.com>
2022-09-20 16:19:28 +0700

    version 1.0.0

---
+++ CultureInfos/CultureInfos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace CultureInfos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Displays several properties of the neutral cultures.
+            Console.WriteLine("CULTURE ISO ISO WIN DISPLAYNAME                              ENGLISHNAME");
+            foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                Console.Write("{0,-7}", ci.Name);
+                Console.Write(" {0,-3}", ci.TwoLetterISOLanguageName);
+                Console.Write(" {0,-3}", ci.ThreeLetterISOLanguageName);
+                Console.Write(" {0,-3}", ci.ThreeLetterWindowsLanguageName);
+                Console.Write(" {0,-40}", ci.DisplayName);
+                Console.WriteLine(" {0,-40}", ci.EnglishName);
+            }
+        }
+    }
+}
